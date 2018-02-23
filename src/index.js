a03428b823509840ef5825c7d556864023f4229e
Cooper Hanke <hanke31314@gmail.com>
2018-02-23 10:05:29 -0600

    added image asset to begin working on first section

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
+
+
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
