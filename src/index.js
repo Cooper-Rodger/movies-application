21649b323f2146d351ece40a70f987ca79be9e4b
Cooper Hanke <hanke31314@gmail.com>
2018-02-23 12:15:40 -0600

    added functionality to add movies via json method

--- src/index.js
+++ src/index.js
@@ -9,13 +9,39 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 const loading = document.getElementById('loading-setpoint');
+const title = document.getElementById('title');
+const rating = document.getElementById('rating');
+const buttonAdd = document.getElementById('buttonAdd');
+
+function addMovie () {
+    let blogPost = {title: title.value, rating: rating.value};
+
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(blogPost),
+    };
+    fetch(url, options)
+        .then(console.log(response => response.json()))
+        .catch(/* handle errors */);
+}
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    loading.innerHTML = ('');
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        loading.innerHTML += (`<p>id#${id} - ${title} - rating: ${rating}</p>`);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+});
+
+buttonAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(title.value);
+    addMovie();
 });
