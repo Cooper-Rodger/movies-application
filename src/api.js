29589186ab74942db084ef20c8040334405456d2
RodgerScott <rodger.scott@gmail.com>
2018-02-23 11:37:14 -0600

    Completed onload portion of functionality. Working on adding movies through post.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 module.exports = {
   getMovies: () => {
-    return fetch('/api/movies')
+  const loading = document.getElementById('loading-setpoint');
+  loading.innerHTML = ('<div class="loading-gif"><img src="img/loading.gif" alt="loading movie items"/></div>');
+      return fetch('/api/movies')
       .then(response => response.json());
   }
 };
--- src/index.js
+++ src/index.js
@@ -9,13 +9,22 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 const loading = document.getElementById('loading-setpoint');
+const title = document.getElementById('title');
+const rating = document.getElementById('rating');
+const buttonAdd = document.getElementById('buttonAdd');
 
 getMovies().then((movies) => {
+  loading.innerHTML = ('');
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    loading.innerHTML += (`<p>id#${id} - ${title} - rating: ${rating}</p>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+buttonAdd.addEventListener('click', (e) => {
+  console.log("test");
+  e.preventDefault();
 });
