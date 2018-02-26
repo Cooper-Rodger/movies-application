15336a42263775c9e12b3fe935c042fd0665436e
Cooper Hanke <hanke31314@gmail.com>
2018-02-26 10:24:02 -0600

    in process of modifying editing functionality for autofill

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-/**
- * require style imports
- */
-
 const $ = require('jQuery');
 const {getMovies} = require('./api.js');
 const loading = document.getElementsByClassName('loading-setpoint').item(0);
 const add = document.getElementById('add');
 const edit = document.getElementById('edit');
-const remove = document.getElementById('remove');
-const movieItem = document.getElementById('movieList');
-let test = undefined;
-
-// const buttons = document.getElementById('buttons-group');
+let selectedMovie = undefined;
 
 refresh(); // refreshes the movie list upon initially loading the page
 
@@ -29,17 +21,39 @@ function addMovieSetup () { // actual function that allows us to add a movie via
     });
 }
 
-function editMovie(input) {
-    console.log(input);
+function editMovie(movieToEdit) {
+    console.log(movieToEdit);
+    const id = movieToEdit.attr('id');
+    const currentTitle = movieToEdit.attr('title');
+    console.log(currentTitle);
+    const currentRating = movieToEdit.attr('rating');
+    console.log(currentRating);
+    console.log(id);
     const editForm = `<div class="editForm form">` +
         `<form action="../db.json" method="POST">` +
-        `<label for="title">Title <input type="text" id="title" name="editMovie" value=""></label>` +
-        `<label for="rating">Rating <input type="text" id="rating" name="editMovie"></label>` +
+        `<label for="title">Title <input type="text" id="edit-title-${id}" name="editMovie" value="${currentTitle}"></label>` +
+        `<label for="rating">Rating <input type="text" id="edit-rating-${id}" name="editMovie" value="${currentRating}"></label>` +
         `<button id="buttonEdit">Submit Changes</button></form></div>`;
-    input.append(editForm);
+    movieToEdit.append(editForm);
     document.getElementById('buttonEdit').addEventListener('click', (e) => {
         e.preventDefault();
         $('.editForm').fadeOut();
+        const url = `/api/movies/${id}`;
+        console.log(url);
+        const title = document.getElementById('edit-title-' + id);
+        const rating = document.getElementById('edit-rating-' + id);
+        const movieToSubmit = { title: title.value, rating: rating.value};
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieToSubmit),
+        };
+        fetch(url, options)
+            .then(() => refresh())
+            .catch(() => alert(`Something went wrong, and everyone dies. The end.`));
+
         console.log(`normally would go through => processEditedMovie() but not now`);
     });
 }
@@ -61,17 +75,39 @@ function processAddedMovie() { // sends this movie request to the server and upd
         .catch(() => alert(`Something went wrong, and everyone dies. The end.`));
 }
 
+function removeMovie(movieToRemove) {
+    console.log(movieToRemove);
+    const id = movieToRemove.attr('id');
+    // fetch the list of movies again
+    const url = `/api/movies/${id}`;
+    console.log(url);
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieToRemove),
+    };
+    fetch(url, options)
+        .then(() => refresh())
+        .catch(() => alert(`Something went wrong, and everyone dies. The end.`));
+    // use the id as a key value pair to find the movie that matches (maybe use .filter() ? )
+};
+
 function refresh() { // function that processes movies from the api request response from the server
     getMovies().then((movies) => {
+        let temp = '';
         loading.innerHTML = ('');
         console.log('Here are all the movies:');
-        loading.innerHTML = (`<ul id="moviesList">`);
-        movies.forEach(({title, rating}) => {
-            loading.innerHTML += (`<li>${title}<br>rating: ${rating}</li>` +
+        temp += (`<ul id="moviesList">`);
+        movies.forEach(({title, rating, id}) => {
+            temp += (`<li id="${id}"><span id=" ">${title}</span><br>rating: ${rating}<span class="hidden ident">${id}</span></li>` +
                 `<div class="hidden menu"><button class="edit">Edit</button>` +
-                `<button class="remove">Remove</button></div></li>`);
+                `<button class="remove">Remove</button></div>`);
         });
-        loading.innerHTML += (`</ul>`);
+        temp += (`</ul>`);
+        console.log(temp);
+        loading.innerHTML = temp;
         $('#add-movie').fadeIn();
     }).catch((error) => {
 
@@ -88,25 +124,13 @@ add.addEventListener('click', (e) => {
 
 $(".loading-setpoint").on("click", "li", function() {
     $(this).next().toggleClass('hidden');
-    test = ($(this));
+    selectedMovie = ($(this));
     // $(this).children('li').toggleClass('hidden');
 }).on("click", ".edit", function() {
-    editMovie(test);
+    editMovie(selectedMovie);
 }).on("click", ".remove", function() {
-    console.log('would have removed this');
+    removeMovie(selectedMovie);
 });
-
-//
-// $("#loading-setpoint").on("click", ".edit", function() {
-//     console.log('hit edit');
-//     console.log($(this).parent().parent().children());
-//     // should go to function that will allow us to edit the inner html
-// });
-//
-// $("#loading-setpoint").on("click", ".remove", function() {
-//     console.log('hit remove');
-//     // should go to function that will allow us to remove the parent it is in.
-// });
 
 
 
